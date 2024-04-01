6f040e6a5a83179dc1620779ea6090be683f64e7
Killian QUINTIN <killian.quintin@epitech.eu>
2024-04-01 23:39:18 +0200

    [FIX] parse_str()

--- lib/my/parse_str.c
+++ lib/my/parse_str.c
@@ -11,14 +11,14 @@
 char *parse_str(char const *str, int ind_start, int ind_end)
 {
     char *ans;
-    int len = ind_end - ind_start + 1;
+    int len = ind_end - ind_start;
     int ind = 0;
 
-    if (ind_end < 0 || ind_start < 0 || len < 0
-    || ind_end >= my_strlen(str))
+    if (str == NULL || ind_end < 0 || ind_start < 0 || len <= 0
+    || ind_end > my_strlen(str))
         return NULL;
     ans = malloc(sizeof(char) * (len + 1));
-    for (int i = ind_start; i <= ind_end; i++) {
+    for (int i = ind_start; i < ind_end; i++) {
         ans[ind] = str[i];
         ind++;
     }
