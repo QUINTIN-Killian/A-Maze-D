97119b6e7f8b54ad3b80d56394324f2ee37302dc
Killian QUINTIN <killian.quintin@epitech.eu>
2024-04-01 16:06:59 +0200

    [ADD] first prototype of main struct

--- include/amazed.h
+++ include/amazed.h
@@ -14,4 +14,20 @@ typedef enum boolean {
     True
 } bool;
 
+typedef struct robot_s {
+    int current_room;
+} robot_t;
+
+typedef struct linked_room_s {
+    int id;
+    struct linked_room_s *next;
+    int *close_rooms;
+} linked_room_t;
+
+typedef struct amazed_s {
+    int nb_robot;
+    robot_t **tab_robot;
+    linked_room_t *room;
+} amazed_t;
+
 #endif
