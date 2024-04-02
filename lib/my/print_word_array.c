55536e59156cb4c18c754785c11d91881741e606
Killian QUINTIN <killian.quintin@epitech.eu>
2024-04-02 15:37:55 +0200

    [FIX] print_word_array()

--- lib/my/print_word_array.c
+++ lib/my/print_word_array.c
@@ -10,6 +10,10 @@
 
 void print_word_array(char **word_array)
 {
+    if (word_array == NULL) {
+        mini_printf("NULL\n");
+        return;
+    }
     for (int i = 0; i < my_strlen_array(word_array); i++) {
         mini_printf("%s\n", word_array[i]);
     }
